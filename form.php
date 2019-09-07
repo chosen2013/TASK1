27c38726f5b5f1327350fc3a1ed02d1d6a05464e
chosen2013 <38971960+chosen2013@users.noreply.github.com>
2019-09-08 00:28:09 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -1,11 +1,10 @@
 <?php
-    $title = $_POST["channel0Title"]; //You have to get the form data
-    $gain = $_POST["channel0Gain"];
-    $offset = $_POST["channel0Offset"];
-    $file = fopen('response.txt', 'w+'); //Open your .txt file
-    ftruncate($file, 0); //Clear the file to 0bit
-    $content = $title. PHP_EOL .$gain. PHP_EOL .$offset;
-    fwrite($file , $content); //Now lets write it in there
-    fclose($file ); //Finally close our .txt
-    die(header("Location: ".$_SERVER["HTTP_REFERER"]));
+              
+if(isset($_POST['textdata']))
+{
+$data=$_POST['textdata'];
+$fp = fopen('data.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
 ?>
